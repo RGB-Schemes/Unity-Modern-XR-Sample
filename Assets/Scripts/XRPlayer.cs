7b7705952e4fa7ba0b04d15a727642179ccf6627
Gerald McAlister <gemisis@users.noreply.github.com>
2020-02-04 18:09:30 -0800

    Project Commit
    [Problem]
    Entire repository is empty!
    
    [Solution]
    Added the initial project and its files.

---
+++ Assets/Scripts/XRPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class XRPlayer : MonoBehaviour
+{
+    public GameObject Head;
+    public Rigidbody LeftHand, RightHand;
+
+    private List<XRNodeState> mNodeStates = new List<XRNodeState>();
+    private Vector3 mHeadPos, mLeftHandPos, mRightHandPos;
+    private Quaternion mHeadRot, mLeftHandRot, mRightHandRot;
+
+    private void Start()
+    {
+        List<XRInputSubsystem> subsystems = new List<XRInputSubsystem>();
+        SubsystemManager.GetInstances<XRInputSubsystem>(subsystems);
+        for (int i = 0; i < subsystems.Count; i++)
+        {
+            subsystems[i].TrySetTrackingOriginMode(TrackingOriginModeFlags.Floor);
+        }
+    }
+
+    private void Update()
+    {
+        InputTracking.GetNodeStates(mNodeStates);
+
+        foreach (XRNodeState nodeState in mNodeStates)
+        {
+            switch (nodeState.nodeType)
+            {
+                case XRNode.Head:
+                    nodeState.TryGetPosition(out mHeadPos);
+                    nodeState.TryGetRotation(out mHeadRot);
+                    break;
+            }
+        }
+        Head.transform.position = mHeadPos;
+        Head.transform.rotation = mHeadRot.normalized;
+    }
+
+    // FixedUpdate stays in sync with the physics engine.
+    private void FixedUpdate()
+    {
+        InputTracking.GetNodeStates(mNodeStates);
+
+        foreach (XRNodeState nodeState in mNodeStates)
+        {
+            switch (nodeState.nodeType)
+            {
+                case XRNode.LeftHand:
+                    nodeState.TryGetPosition(out mLeftHandPos);
+                    nodeState.TryGetRotation(out mLeftHandRot);
+                    break;
+                case XRNode.RightHand:
+                    nodeState.TryGetPosition(out mRightHandPos);
+                    nodeState.TryGetRotation(out mRightHandRot);
+                    break;
+            }
+        }
+
+        LeftHand.MovePosition(mLeftHandPos);
+        LeftHand.MoveRotation(mLeftHandRot.normalized);
+        RightHand.MovePosition(mRightHandPos);
+        RightHand.MoveRotation(mRightHandRot.normalized);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Unity-Modern-XR-Sample
-An example of how to setup a virtual reality project using Unity's modern XR systems! You can view the full tutorial for this repository at https://rgbschemes.com/blog/setting-up-unity-vr-2020/
+# Unity Modern XR Sample
+
+This tutorial covers how to setup a virtual reality project using Unity's new [XR system](https://docs.unity3d.com/Manual/XR.html). It specifically shows how to do so without using any of Unity's legacy tools **and** without any third party assets! This means it is incredibly lightweight, requiring only a single script to function. The full tutorial can be found at [our website](https://rgbschemes.com/blog/setting-up-unity-xr-in-2020/).
+
+![GIF of the final result](https://github.com/RGB-Schemes/Unity-Modern-XR-Sample/raw/master/result.gif "The final results")
